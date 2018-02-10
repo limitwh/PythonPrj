cbd08062445e46640f19b4e5401ea20eaa138057
limitwh <limitwh@163.com>
2018-02-11 01:25:06 +0800

    添加数据库DDL文件
    DDL中建立两张表

---
+++ DB.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE JDdb;
+
+USE JDdb;
+
+CREATE TABLE CURRENT(
+CurrentId int(10) NOT NULL auto_increment,
+CurItemId int(20) NOT NULL UNIQUE,
+ItemName varchar(20) NOT NULL,
+URL varchar(128) NOT NULL UNIQUE,
+CurPrice double(10,2) NOT NULL,
+UpdTimeVersion timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY  (CurrentId)
+);
+
+CREATE TABLE HISTORY(
+HistoryId  int(20) NOT NULL auto_increment,
+HisItemId int(20) NOT NULL UNIQUE,
+HisPrice double(10,2) NOT NULL ,
+GetTimeVersion timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY  (HistoryId)
+);
