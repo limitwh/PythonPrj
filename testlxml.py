28df046672f46c28aeba1b10ac25efabf6adfc26
limitwh <limitwh@163.com>
2018-02-14 23:02:52 +0800

    解决了lxml里的etree找不到的问题
    lxml.py与lxml模块重名

--- testlxml.py
+++ testlxml.py
@@ -6,5 +6,5 @@ from lxml import etree
 Url="https://item.jd.com/3888216.html"
 UrlStr=requests.get(Url)
 tree = etree.parse(UrlStr)
-r = tree.XPath("/html/body/div[11]/div[2]/div[1]/div[2]/div[1]/div[1]/ul[3]/li[1]")
+r = tree.XPath("/html/body/div[11]/div[2]/div[1]/div[2]/div[1]/div[1]/ul[3]/li[1]").extract()
 len(r)
