e604abca5beb600f70d7e9670673c83372cf1c9d
limitwh <limitwh@163.com>
2018-02-17 23:16:35 +0800

    xpath可用，但是无法取得元素内容
    怀疑JD对商品详情使用了ajax

--- ItemPrice.py
+++ ItemPrice.py
@@ -2,8 +2,6 @@ __author__ = 'limitwh'
 #coding=utf-8
 import requests
 import json
-#Itemres=requests.get("https://p.3.cn/prices/mgets?skuIds=J_5544080").json()
-#print(Itemres[0]['op'])
 
 def GetPrice(str):
 	PriceUrl = "https://p.3.cn/prices/mgets?skuIds=J_"+str
--- testlxml.py
+++ testlxml.py
@@ -4,7 +4,9 @@ import requests
 from lxml import etree
 
 Url="https://item.jd.com/3888216.html"
-UrlStr=requests.get(Url)
-tree = etree.parse(UrlStr)
-r = tree.XPath("/html/body/div[11]/div[2]/div[1]/div[2]/div[1]/div[1]/ul[3]/li[1]").extract()
-len(r)
+UrlStr=requests.get(Url).content
+html=etree.HTML(UrlStr)
+print(UrlStr.decode('unicode_escape'))
+#tree = etree.parse(UrlStr)
+#r = tree.XPath("/html/body/div[11]/div[2]/div[1]/div[2]/div[1]/div[1]/ul[3]/li[1]")
+#print(len(r))
