09693f9915a935bb33fbc472df8c25089942520c
limitwh <limitwh@163.com>
2018-02-24 01:27:50 +0800

    调试插入SQL语句

--- DB.sql
+++ DB.sql
@@ -7,18 +7,18 @@ Drop TABLE HISTORY;
 
 CREATE TABLE CURRENT(
 CurrentId int(20) NOT NULL auto_increment,
-CurItemId int(20) NOT NULL UNIQUE,
-ItemName varchar(20) NOT NULL,
+CurItemId int(40) NOT NULL UNIQUE,
+ItemName varchar(120) NOT NULL,
 URL varchar(128) NOT NULL UNIQUE,
 CurPrice double(10,2) NOT NULL,
 UpdTimeVersion timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY  (CurrentId)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE HISTORY(
 HistoryId  int(20) NOT NULL auto_increment,
-HisItemId int(20) NOT NULL UNIQUE,
+HisItemId int(40) NOT NULL UNIQUE,
 HisPrice double(10,2) NOT NULL ,
 GetTimeVersion timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY  (HistoryId)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- InsterMysql.py
+++ InsterMysql.py
@@ -2,18 +2,15 @@ __author__ = 'limitwh'
 #coding=utf-8
 import pymysql
 ItemList=[['19994179320', 'https://item.jd.com/19994179320.html', '华为（HUAWEI）Mate 10 Pro', '5399.00'], 
-          ['21847456363', 'https://item.jd.com/21847456363.html', '华为（HUAWEI）荣耀V9 play', '1199.00'], 
-          ['18377347431', 'https://item.jd.com/18377347431.html', '华为（HUAWEI） 荣耀 畅玩7X 全网通4G智能手机 铂光金 （4G+128G） 尊享版', '1999.00']]
-db=pymysql.connect(host="localhost",user="pytest",password="password",db="JDdb",port=3306,charset='utf8')
-InsterSql="""INSERT INTO CURRENT (CurItemId,ItemName,URL,CurPrice) VALUES(%s,%s,%s,%s)"""
+          ['21847456363', 'https://item.jd.com/21847456363.html', '华为（HUAWEI）荣耀V9 play', '1199.00']] 
+#          ['18377347431', 'https://item.jd.com/18377347431.html', '华为（HUAWEI） 荣耀 畅玩7X 全网通4G智能手机 铂光金 （4G+128G） 尊享版', '1999.00']]
+db=pymysql.connect(host="localhost",user="pytest",password="password",db="JDdb",port=3306,charset='gbk')
+InsterSql="INSERT INTO CURRENT (CurItemId,URL,ItemName,CurPrice) VALUES (%s,%s,%s,%s)"
 cur = db.cursor() 
-print(InsterSql)
 try:  
-    sts=cur.execute(InsterSql) 
-    if sts==1:
-        print("Done")
-    else:
-        print("Failed")
+    for Item in ItemList:
+        print(InsterSql)
+        cur.execute(InsterSql,(Item[0],Item[1],Item[2],Item[3]))
     db.commit()
 except Exception as e:  
     db.rollback()
